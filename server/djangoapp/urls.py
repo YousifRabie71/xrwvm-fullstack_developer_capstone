f6441e4c282211f469391b9f0afdb25d7153a1b9
YousifRabie71 <ymaf15612@gmail.com>
2026-06-07 11:18:02 -0400

    Add get cars endpoint and populate car data

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -3,20 +3,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
-app_name = 'djangoapp'
+app_name = "djangoapp"
 
 urlpatterns = [
-    # path for registration
-    path(route='register', view=views.registration, name='register'),
-
-    # path for login
-    path(route='login', view=views.login_user, name='login'),
-
-    # path for logout
-    path(route='logout', view=views.logout_request, name='logout'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
+    path(route="login", view=views.login_user, name="login"),
+    path(route="logout", view=views.logout_request, name="logout"),
+    path(route="register", view=views.registration, name="register"),
+    path(route="get_cars", view=views.get_cars, name="getcars"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,18 +1,13 @@
-# Uncomment the required imports before adding the code
-
-# from django.shortcuts import render
-# from django.http import HttpResponseRedirect, HttpResponse
-from django.contrib.auth.models import User
-# from django.shortcuts import get_object_or_404, render, redirect
-# from django.contrib import messages
-# from datetime import datetime
-
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
+
 import logging
 import json
-# from .populate import initiate
+
+from .models import CarMake, CarModel
+from .populate import initiate
 
 
 logger = logging.getLogger(__name__)
@@ -21,30 +16,22 @@ logger = logging.getLogger(__name__)
 @csrf_exempt
 def login_user(request):
     data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
+    username = data["userName"]
+    password = data["password"]
 
     user = authenticate(username=username, password=password)
 
-    data = {
-        "userName": username
-    }
-
+    data = {"userName": username}
     if user is not None:
         login(request, user)
-        data = {
-            "userName": username,
-            "status": "Authenticated"
-        }
+        data = {"userName": username, "status": "Authenticated"}
 
     return JsonResponse(data)
 
 
 def logout_request(request):
     logout(request)
-    data = {
-        "userName": ""
-    }
+    data = {"userName": ""}
     return JsonResponse(data)
 
 
@@ -52,18 +39,18 @@ def logout_request(request):
 def registration(request):
     data = json.loads(request.body)
 
-    username = data['userName']
-    password = data['password']
-    first_name = data['firstName']
-    last_name = data['lastName']
-    email = data['email']
+    username = data["userName"]
+    password = data["password"]
+    first_name = data["firstName"]
+    last_name = data["lastName"]
+    email = data["email"]
 
     username_exist = False
 
     try:
         User.objects.get(username=username)
         username_exist = True
-    except:
+    except User.DoesNotExist:
         logger.debug("{} is new user".format(username))
 
     if not username_exist:
@@ -72,37 +59,30 @@ def registration(request):
             first_name=first_name,
             last_name=last_name,
             password=password,
-            email=email
+            email=email,
         )
-
         login(request, user)
-
-        data = {
-            "userName": username,
-            "status": "Authenticated"
-        }
-
-        return JsonResponse(data)
-    else:
-        data = {
-            "userName": username,
-            "error": "Already Registered"
-        }
-
+        data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
 
-
-# def get_dealerships(request):
-# ...
-
-
-# def get_dealer_reviews(request, dealer_id):
-# ...
+    data = {"userName": username, "error": "Already Registered"}
+    return JsonResponse(data)
 
 
-# def get_dealer_details(request, dealer_id):
-# ...
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
 
+    if count == 0:
+        initiate()
 
-# def add_review(request):
-# ...
+    car_models = CarModel.objects.select_related("car_make")
+    cars = []
+
+    for car_model in car_models:
+        cars.append({
+            "CarModel": car_model.name,
+            "CarMake": car_model.car_make.name
+        })
+
+    return JsonResponse({"CarModels": cars})
